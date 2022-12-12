d0162ba7f8c403f9564b9f288f702c4f5ab40baa
areekaras <shibili.areekara@gmail.com>
2022-12-12 13:39:36 +0530

    - Set up login vc storyboard and view controller

---
+++ JetDevsHomeWork/Login/ViewController/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  JetDevsHomeWork
+//
+//  Created by Shibili Areekara on 12/12/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeButtonTap(_ sender: AnyObject) {
+        self.dismiss(animated: true)
+    }
+}
