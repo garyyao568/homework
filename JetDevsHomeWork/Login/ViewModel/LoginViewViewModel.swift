34177aa48b4e7b4701d3fb09f6491d818ee00d86
areekaras <shibili.areekara@gmail.com>
2022-12-12 21:11:15 +0530

    - Add `LoginViewViewModel` and authenticate function

---
+++ JetDevsHomeWork/Login/ViewModel/LoginViewViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewViewModel.swift
+//  JetDevsHomeWork
+//
+//  Created by Shibili Areekara on 12/12/22.
+//
+
+import Foundation
+
+class LoginViewViewModel {
+    let authenticator: JDAuthenticator
+    
+    init(authenticator: JDAuthenticator) {
+        self.authenticator = authenticator
+    }
+    
+    func authenticate(completion: @escaping (Error?) -> Void) {
+        let params = JDAuthenticatorParams(email: "test@jetdevs.com",
+                                           password: "Jetdevs2021")
+        
+        authenticator.login(params: params) { result in
+            switch result {
+            case .success:
+                completion(nil)
+            case let .failure(error):
+                completion(error)
+            }
+        }
+    }
+}
