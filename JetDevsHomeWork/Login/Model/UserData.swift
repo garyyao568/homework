33748ba1965e64acaa14b475025e8ec8f6819344
OSMAnand <anand.p@osmosys.co>
2024-02-03 17:10:53 +0530

    feat - create User data model
    Create user data model as per the response
    Created another user data model to save data locally

---
+++ JetDevsHomeWork/Login/Model/UserData.swift
@@ -0,0 +1,16 @@
+//
+//  UserData.swift
+//  JetDevsHomeWork
+//
+//  Created by Anand Prakash on 03/02/24.
+//
+
+import Foundation
+
+struct UserData {
+    
+    let userID: Int
+    let name: String
+    let profileURL: String
+    let createdAt: String
+}
