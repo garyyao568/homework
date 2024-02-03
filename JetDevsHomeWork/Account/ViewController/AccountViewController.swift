6585f3897bce4f24f12896a055f429ba54bddd62
OSMAnand <anand.p@osmosys.co>
2024-02-03 21:30:56 +0530

    feat - Create AccountViewModel
    1. Create AccountViewModel
    2. assign value to respective field and dismiss Login screen

--- JetDevsHomeWork/Account/ViewController/AccountViewController.swift
+++ JetDevsHomeWork/Account/ViewController/AccountViewController.swift
@@ -15,6 +15,9 @@ class AccountViewController: UIViewController {
 	@IBOutlet weak var daysLabel: UILabel!
 	@IBOutlet weak var nameLabel: UILabel!
 	@IBOutlet weak var headImageView: UIImageView!
+    
+    var viewModel = AccountsViewModel()
+
 	override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,5 +32,41 @@ class AccountViewController: UIViewController {
             navigationController?.present(loginVC, animated: true)
         }
 	}
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        initView()
+    }
+    
+    private func initView() {
+        viewModel.loadUser { [weak self] user in
+            guard let self = self else {
+                return
+            }
+            
+            if let user = user {
+                self.show(login: false)
+                self.loadUserData(user: user)
+            } else {
+                self.show(login: true)
+            }
+        }
+    }
+    
+    private func loadUserData(user: UserLocalData) {
+        nameLabel.text = user.name
+        daysLabel.text = viewModel.getNumberOfDays(from: user.createdAt)
+        
+        if !user.profileURL.isEmpty,
+           let url = URL(string: user.profileURL) {
+            let placeholder = UIImage(named: "Avatar")
+            headImageView.kf.setImage(with: url, placeholder: placeholder)
+        }
+    }
+    
+    private func show(login: Bool) {
+        nonLoginView.isHidden = !login
+        loginView.isHidden = login
+    }
 	
 }
